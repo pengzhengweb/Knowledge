5d76e9fb26f0aa0ccfd8ad88c36b178c3c70f794
james king <fepengzheng767@gmail.com>
2020-02-03 17:36:24 +0800

    something useless

--- Desktop/Iview/async.js
+++ Desktop/Iview/async.js
@@ -1,17 +1,67 @@
-setTimeout(function() {
-    console.log('setTimeout');
-}, 0)
+// setTimeout(function() {
+//     console.log('setTimeout');
+// }, 0)
+// async function async1() {
+//     console.log('async1 start');
+//     await async2();
+//     console.log('async1 end');
+// }
+// async function async2() {
+//     console.log('async2');
+// }
+
+// async1();
+// console.log('script start');
+// new Promise(function(resolve) {
+//     console.log('promise1');
+//     resolve();
+// }).then(function() {
+//     console.log('promise2');
+// });
+// console.log('script end');
+
+// async function async1() {
+//     console.log('async1 start');
+//     await async2();
+//     console.log('async1 end');
+// }
+// async function async2() {
+//     console.log('async2');
+// }
+// console.log('script start');
+// setTimeout(function() {
+//     console.log('setTimeout');
+// }, 0)
+// async1();
+// new Promise(function(resolve) {
+//     console.log('promise1');
+//     resolve();
+// }).then(function() {
+//     console.log('promise2');
+// });
+// console.log('script end');
+
 async function async1() {
     console.log('async1 start');
     await async2();
-    console.log('async1 end');
+    //更改如下：
+    setTimeout(function() {
+        console.log('setTimeout1')
+    }, 0)
 }
 async function async2() {
-    console.log('async2');
+    //更改如下：
+    setTimeout(function() {
+        console.log('setTimeout2')
+    }, 0)
 }
-
-async1();
 console.log('script start');
+
+setTimeout(function() {
+    console.log('setTimeout3');
+}, 0)
+async1();
+
 new Promise(function(resolve) {
     console.log('promise1');
     resolve();
--- Desktop/Iview/settimeout.js
+++ Desktop/Iview/settimeout.js
@@ -1,28 +1,24 @@
-console.log("begin");
-setTimeout(() => {
-        console.log('setTimeout1');
-        Promise.resolve(1).then(() => {
-            console.log('Promise1');
-            setTimeout(() => {
-                console.log("Promise1 && Promise2");
-            }, 0)
-        }).then(() => {
-            console.log("Promise2");
-        })
+// console.log("begin");
+// setTimeout(() => {
+//         console.log('setTimeout1');
+//         Promise.resolve(1).then(() => {
+//             console.log('Promise1');
+//             setTimeout(() => {
+//                 console.log("Promise1 && Promise2");
+//             }, 0)
+//         }).then(() => {
+//             console.log("Promise2");
+//         })
+//     }, 0)
+console.log("end");
+//console.log(global);
+setTimeout(() => console.log(1), 0);
+console.log(2);
+new Promise((resolve, reject) => {
+    console.log(3);
+    setTimeout(() => {
+        console.log(6);
+        resolve();
     }, 0)
-    // console.log("end");
-    //console.log(global);
-    // setTimeout(() => console.log(1), 0);
-    // console.log(2);
-    // new Promise((resolve, reject) => {
-    //     console.log(3);
-    //     setTimeout(() => {
-    //         console.log(6);
-    //         resolve();
-    //     }, 0)
 
-// }).then(() => {
-//     console.log(4);
-// }).then(() => {
-//     console.log(5);
-// })
+})
