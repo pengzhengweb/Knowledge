f6d9f9d7bf639df5d8ac6355eb8f1ccbc123c910
james king <fepengzheng767@gmail.com>
2020-02-18 17:37:07 +0800

    right

--- Desktop/Iview/interview.js
+++ Desktop/Iview/interview.js
@@ -1,21 +1,24 @@
 let count = 0;
-query().then(res => {
-    console.log(res); //{state:1,data:{}}
-}, (res) => {
-    const interval = 3000;
-
-    let loop = () => {
-        setTimeout(() => {
-            query()
-        }, interval)
-    };
-
-    if (res.state === 0) {
-        count++;
-        count === 100 ? Promise.reject(res.reason) : loop();
-    } else if (res.state === 1) {
-        Promise.resolve(res);
-    } else {
-        throw new Error('something bad happened');
-    }
-})
+const interval = 3000;
+let loop = () => {
+    query().then(res => {
+        console.log(res); //{state:1,data:{}}
+    }, (res) => {
+        console.log(res); //{state:0,data:{}}
+        timer && clearTimeout(timer);
+        if (res.state === 0) {
+            count++;
+            if (count === 100) {
+                Promise.reject(res.reason)
+            } else {
+                timer && clearTimeout(timer);
+                let timer = setTimeout(loop(), interval);
+            }
+        } else if (res.state === 1) {
+            Promise.resolve(res);
+        } else {
+            throw new Error('something bad happened');
+        }
+    })
+};
+loop();
