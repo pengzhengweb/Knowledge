7a243f1a4b7438ddeb75b1f2827c848f19612818
james king <fepengzheng767@gmail.com>
2020-02-21 09:41:22 +0800

    practise

--- Desktop/Iview/byteDance.js
+++ Desktop/Iview/byteDance.js
@@ -1,26 +1,26 @@
-function addLabel(tree) {
-    function tool(original, current, flag = true) {
-        let res = ''
-        if (original) {
-            res = `${original}.` + `${current}`
-        } else {
-            res = `${current}`
-        }
+// function addLabel(tree) {
+//     function tool(original, current, flag = true) {
+//         let res = ''
+//         if (original) {
+//             res = `${original}.` + `${current}`
+//         } else {
+//             res = `${current}`
+//         }
 
-        return flag ? `(${res})` : res
-    }
+//         return flag ? `(${res})` : res
+//     }
 
-    function getTitle(items, result = [], index = '') {
-        items.forEach((item, i) => {
-            result.push(tool(index, i + 1) + item.name);
-            if (item.children) {
-                getTitle(item.children, result, tool(index, i + 1, false));
-            }
-        })
-        return result;
-    }
-    return getTitle(tree.children || [], [tree.name]);
-}
+//     function getTitle(items, result = [], index = '') {
+//         items.forEach((item, i) => {
+//             result.push(tool(index, i + 1) + item.name);
+//             if (item.children) {
+//                 getTitle(item.children, result, tool(index, i + 1, false));
+//             }
+//         })
+//         return result;
+//     }
+//     return getTitle(tree.children || [], [tree.name]);
+// }
 var chapterTree = {
     name: '总章节',
     children: [{
@@ -47,3 +47,26 @@ var chapterTree = {
 
 var demo = addLabel(chapterTree);
 console.log(demo);
+
+function addLabel(tree) {
+    function tool(orginal, current, flag = true) {
+        let res = '';
+        if (orginal) {
+            res = `${orginal}.` + `${current}`;
+        } else {
+            res = `${current}`;
+        }
+        return flag ? `(${res})` : res;
+    }
+
+    function getTitle(items, result = [], index = '') {
+        items.forEach((item, i) => {
+            result.push(tool(index, i + 1) + item.name);
+            if (item.children) {
+                getTitle(item.children, result, tool(index, i + 1, false));
+            }
+        })
+        return result;
+    }
+    return getTitle(tree.children || [], [tree.name]);
+}
